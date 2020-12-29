8e98cac4daf1dc8b69b3f778c2eb3004bcfa7ed4
0xflotus <0xflotus@gmail.com>
2020-12-29 17:33:58 +0100

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
 pub mod util;
+
+#[test]
+fn test() {
+    assert!(util::cbb::int_to_bal_ternary(1) == "000+");
+    assert!(util::cbb::int_to_bal_ternary(2) == "00+-");
+    assert!(util::cbb::int_to_bal_ternary(3) == "00+0");
+    assert!(util::cbb::int_to_bal_ternary(17) == "+-0-");
+    assert!(util::cbb::int_to_bal_ternary(20) == "+-+-");
+    assert!(util::cbb::int_to_bal_ternary(200) == "+-+++-");
+}
