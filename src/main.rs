5687e6781d186ba92e2917f3821feddf8c65db24
0xflotus <0xflotus@gmail.com>
2020-12-30 05:22:09 +0100

    v0.1.8

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use util::cbb;
 
 fn main() {
     let matches = App::new("cbb")
-        .version("0.1.7")
+        .version("0.1.8")
         .about("A converter for numbers")
         .setting(AppSettings::AllowLeadingHyphen)
         .arg(
